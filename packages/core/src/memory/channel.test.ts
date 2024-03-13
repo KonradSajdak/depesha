c12cacda5a0813e0c922918b4d33fa45b8226d42
Konrad Sajdak <konrad.sajdak@yellowgray.pl>
2024-03-13 01:27:35 +0100

    allow channel to buffer messages

--- packages/core/src/memory/channel.test.ts
+++ packages/core/src/memory/channel.test.ts
@@ -20,4 +20,18 @@ describe("Channel", () => {
     const outputStreamB = inputStream.map(() => consumerB.pull())
     expect(await Promise.all(outputStreamB)).toEqual(inputStream)
   })
+
+  test("should buffer pushing messages when any consumer registered", async () => {
+    // given
+    const channel = new Channel<string>()
+    const inputStream = ["A", "B", "C", "D"]
+
+    // when
+    inputStream.forEach(message => channel.push(message))
+
+    // then
+    const consumer = channel.consume()
+    const outputStream = inputStream.map(() => consumer.pull())
+    expect(await Promise.all(outputStream)).toEqual(inputStream)
+  })
 })
--- packages/core/src/memory/channel.ts
+++ packages/core/src/memory/channel.ts
@@ -1,19 +1,25 @@
 import { Stream, StreamConsumer, StreamProducer } from "./stream"
 
 export class Channel<T> implements StreamProducer<T> {
+  private readonly buffer: T[] = []
   private readonly consumers: Stream<T>[] = []
 
   public async push(value: T): Promise<T> {
-    if (this.consumers.length !== 0) {
-      await Promise.all(this.consumers.map(consumer => consumer.push(value)))
+    if (this.consumers.length === 0) {
+      this.buffer.push(value)
+      return Promise.resolve(value)
     }
 
+    await Promise.all(this.consumers.map(consumer => consumer.push(value)))
     return Promise.resolve(value)
   }
 
   public consume(): StreamConsumer<T> {
     const consumer = new Stream<T>()
     this.consumers.push(consumer)
+
+    this.buffer.forEach(message => this.push(message))
+    this.buffer.length = 0
 
     return consumer
   }
