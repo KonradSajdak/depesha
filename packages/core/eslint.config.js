a4f216b6ccd1eeb01faa773f7596ee5f94addfff
Konrad Sajdak <konrad.sajdak@yellowgray.pl>
2024-04-26 14:19:01 +0200

    Update linting and formatting configurations

--- packages/core/eslint.config.js
+++ packages/core/eslint.config.js
@@ -1,28 +1,21 @@
+// @ts-check
+
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
+import prettier from "eslint-config-prettier"
+
 export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.ts", "**/*.js"],
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    plugins: ["@typescript-eslint"],
     rules: {
       indent: ["error", 2],
       "linebreak-style": ["error", "unix"],
       quotes: ["error", "double"],
       semi: ["error", "never"],
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/no-unused-vars": "error"
     },
   },
+  prettier
 ]
